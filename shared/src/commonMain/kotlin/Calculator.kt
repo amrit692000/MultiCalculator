e2e53966cf99e96ed96d5563415a097ffa160f53
amrit692000 <A00301070@mycambrian.ca>
2024-06-04 20:28:30 -0400

    divide fun change

--- shared/src/commonMain/kotlin/Calculator.kt
+++ shared/src/commonMain/kotlin/Calculator.kt
@@ -10,11 +10,9 @@
      fun Muliply(left: Int, right: Int): Int{
          return left*right
      }
-     fun Divide(left: Int, right: Int): Double{
-         if(right==0){
-             throw ArithmeticException("Division by zero is not possible!")
-         }
-         return left.toDouble()/right.toDouble()
+     fun Divide(left: Int, right: Int): Int{
+
+         return left/right
      }
 
  }
